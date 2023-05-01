d567cd291efa7cd56166599919cb06a4cc08df50
Romeo-mz <romeo_martinez@etu.u-bourgogne.fr>
2023-05-01 14:40:04 +0200

    Add login methods to authentificate an user

--- api/controller/authentifier.php
+++ api/controller/authentifier.php
@@ -10,4 +10,34 @@ class Authentifier
         $this->APIlogin = new APIlogin($this);
     }
 
+    public function login($username, $password, $univers){
+
+        $user = $this->DBinterface->getDB()->prepare("SELECT * FROM user WHERE username = :username AND univers = :univers");
+
+        //Association des paramètres
+        $user->bindParam(':username', $username);
+        $user->bindParam(':univers', $univers);
+
+        if(!$user){
+            echo "Error while preparing request";
+            return false;
+        }
+
+        $user->execute();
+
+        $result = $user->fetch(PDO::FETCH_ASSOC);
+
+        if($result && password_verify($password, $result['password'])){
+            $_SESSION['username'] = $username;
+            $_SESSION['univers'] = $univers;
+            $_SESSION['id'] = $result['id'];
+            return 0;
+        }
+        else if($result){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
 }
