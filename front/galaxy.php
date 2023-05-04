398171aa9b4c7e7dd3353aa2dfe46c7df8dfc5e7
Hugoreoo <hugo.gclvs@icloud.com>
2023-05-04 18:21:54 +0200

    refact and fix bugs

--- front/galaxy.php
+++ front/galaxy.php
@@ -1,7 +1,7 @@
 <?php
+session_start();
 
 $_SESSION['id_Univers'] = 1;
-
 $_POST['id_Univers'] = $_SESSION['id_Univers'];
 
 // Default galaxy
@@ -14,18 +14,14 @@ if (!isset($_POST['id_SolarSystem'])) {
     $_POST['id_SolarSystem'] = 1;
 }
 
-echo $_POST['id_Univers'] . "<br>";
-echo $_POST['id_Galaxy'] . "<br>";
-echo $_POST['id_SolarSystem'] . "<br>";
+function fetch_data($id_param, $value) {
+    $data = file_get_contents("http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?$id_param=$value");
+    return json_decode($data, true);
+}
 
-$galaxies = file_get_contents("http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?id_Univers=" . $_POST['id_Univers']);
-$galaxies = json_decode($galaxies, true);
-
-$sys_sols = file_get_contents("http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?id_Galaxy=" . $_POST['id_Galaxy']);
-$sys_sols = json_decode($sys_sols, true);
-
-$planets = file_get_contents("http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?id_SolarSystem=" . $_POST['id_SolarSystem']);
-$planets = json_decode($planets, true);
+$galaxies = fetch_data('id_Univers', $_POST['id_Univers']);
+$sys_sols = fetch_data('id_Galaxy', $_POST['id_Galaxy']);
+$planets = fetch_data('id_SolarSystem', $_POST['id_SolarSystem']);
 
 ?>
 
@@ -43,27 +39,25 @@ $planets = json_decode($planets, true);
         <div class="div-galaxy">
             <h2 class="galaxy-title">Galaxie</h2>
             <div class="div-choose-galaxy">
-                <form action="galaxy.php" method="POST">
+                <form action="#">
                     <label for="id_Galaxy">Choisissez une galaxie: </label>
                     <select name="id_Galaxy" id="id_Galaxy">
                         <?php foreach ($galaxies as $galaxy) { ?>
                             <option <?php if ($galaxy['id'] == $_POST['id_Galaxy']) echo ' selected';?> value="<?php echo $galaxy['id']; ?>"><?php echo $galaxy['nom']; ?></option>
                         <?php } ?>
                     </select>
-                    <input type="submit" value="Go">
                 </form>
         </div>
         <div class="div-sys-sol">
             <h2 class="sys-sol-title">Systeme Solaire</h2>
             <div class="div-choose-sys-sol">
-                <form action="galaxy.php" method="POST">
+                <form action="#">
                     <label for="id_SolarSystem">Choisissez un systeme solaire: </label>
                     <select name="id_SolarSystem" id="id_SolarSystem">
                         <?php foreach ($sys_sols as $sys_sol) { ?>
                             <option <?php if ($sys_sol['id'] == $_POST['id_SolarSystem']) echo ' selected';?> value="<?php echo $sys_sol['id']; ?>"><?php echo $sys_sol['nom']; ?></option>
                         <?php } ?>
                     </select>
-                    <input type="submit" value="Go">
                 </form>
             </div>
         </div>
@@ -94,5 +88,59 @@ $planets = json_decode($planets, true);
 
     </main>
 
+    <script>
+        document.getElementById('id_Galaxy').addEventListener('change', updateSolarSystems);
+        document.getElementById('id_SolarSystem').addEventListener('change', updatePlanets);
+
+        async function updateSolarSystems() {
+            let galaxyId = document.getElementById('id_Galaxy').value;
+            let response = await fetch(`http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?id_Galaxy=${galaxyId}`);
+            let solarSystems = await response.json();
+            let solarSystemSelect = document.getElementById('id_SolarSystem');
+
+            // Clear previous options
+            solarSystemSelect.innerHTML = '';
+
+            // Populate new options
+            solarSystems.forEach(solarSystem => {
+                let option = document.createElement('option');
+                option.value = solarSystem.id;
+                option.text = solarSystem.nom;
+                solarSystemSelect.add(option);
+            });
+
+            // Update planets as well
+            updatePlanets();
+        }
+
+        async function updatePlanets() {
+            let solarSystemId = document.getElementById('id_SolarSystem').value;
+            let response = await fetch(`http://localhost:5550/ESIREMPIRE/api/boundary/APIinterface/APIgalaxy.php?id_SolarSystem=${solarSystemId}`);
+            let planets = await response.json();
+            let planetTable = document.querySelector('.planet-tab');
+
+            // Clear previous table rows
+            while (planetTable.rows.length > 1) {
+                planetTable.deleteRow(-1);
+            }
+
+            // Populate new table rows
+            for (let i = 1; i <= 10; i++) {
+                let row = planetTable.insertRow(-1);
+                let positionCell = row.insertCell(0);
+                let planetCell = row.insertCell(1);
+                let playerCell = row.insertCell(2);
+
+                positionCell.textContent = i;
+
+                planets.forEach(planet => {
+                    if (planet.position == i) {
+                        planetCell.textContent = planet.nom;
+                        playerCell.textContent = planet.pseudo;
+                    }
+                });
+            }
+        }
+    </script>
 </body>
 </html>
