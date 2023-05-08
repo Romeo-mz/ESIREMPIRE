5a495ceab63ad896e0b0f29520ea340423f2e831
Hugoreoo <hugo.gclvs@icloud.com>
2023-05-08 13:25:05 +0200

    rm infra without s

--- front/js/infrastructure/application/application.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "../controllers/controller.js";
-import { View } from "../views/view.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const myController = new Controller();
-    const myView = new View(myController);
-}); 
--- front/js/infrastructure/pattern/observer.js
+++
@@ -1,7 +0,0 @@
-export class Observer
-{
-    notify()
-    {
-        throw new Error("You have to define this function !");
-    }
-}
