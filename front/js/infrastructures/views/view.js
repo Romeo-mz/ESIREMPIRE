d3d0ca44f2688cb4f45c4a307c95ac711c9fad30
Hugoreoo <hugo.gclvs@icloud.com>
2023-05-10 18:53:24 +0200

    edit defense

--- front/js/infrastructures/views/view.js
+++ front/js/infrastructures/views/view.js
@@ -16,9 +16,7 @@ export class View extends Observer
 
     update() 
     {
-        console.log("update");
         const infrastructures = this.#controller.infrastructures;
-        console.log(infrastructures);
 
         infrastructures.forEach(infra => {
 
@@ -43,68 +41,148 @@ export class View extends Observer
         let div_information = this.createOrUpdateElement("div", `div-${prefix}-information-${infrastructure.id}`, "div-infrastructure-information");
         let div_image = this.createOrUpdateElement("div", `div-${prefix}-image-${infrastructure.id}`, "div-infrastructure-image");
         let img = this.createOrUpdateElement("img", `img-${prefix}-${infrastructure.id}`, "img-infrastructure");
-        img.src = this.getImageSrcForType(infrastructure.type);
+        
 
-        let div_information_type = null;
-        let div_information_level = null;
-        let div_information_metal = null;
-        let div_information_energie = null;
-
-        if(prefix === "defense") 
+        if(prefix === "installation") 
         {
-            // this.#type_defense = type_defense;
-            // this.#cout_metal = cout_metal;
-            // this.#cout_energie = cout_energie;
-            // this.#cout_deuterium = cout_deuterium;
-            // this.#temps_construction = temps_construction;
-            // this.#point_attaque = point_attaque;
-            // this.#point_defense = point_defense;
 
-            div_information_type = this.createOrUpdateElement("div", `div-${prefix}-type-${infrastructure.id}`, "div-infrastructure-type", infrastructure.type_defense);
-            div_information_level = this.createOrUpdateElement("div", `div-${prefix}-level-${infrastructure.id}`, "div-infrastructure-level", "Niveau: " + infrastructure.level);
-            div_information_metal = this.createOrUpdateElement("div", `div-${prefix}-metal-${infrastructure.id}`, "div-infrastructure-metal", "Métal: " + infrastructure.metal);
-            div_information_energie = this.createOrUpdateElement("div", `div-${prefix}-energie-${infrastructure.id}`, "div-infrastructure-energie", "Energie: " + infrastructure.energie);
-        }
-        else if(prefix === "installation") 
-        {
+            let div_information_type = null;
+            let div_information_level = null;
+            let div_information_metal = null;
+            let div_information_energie = null;
+            let div_information_deuterium = null;
+
+            img.src = this.getImageSrcForType(infrastructure.type_installation);
+
             div_information_type = this.createOrUpdateElement("div", `div-${prefix}-type-${infrastructure.id}`, "div-infrastructure-type", infrastructure.type_installation);
             div_information_level = this.createOrUpdateElement("div", `div-${prefix}-level-${infrastructure.id}`, "div-infrastructure-level", "Niveau: " + infrastructure.level);
             div_information_metal = this.createOrUpdateElement("div", `div-${prefix}-metal-${infrastructure.id}`, "div-infrastructure-metal", "Métal: " + infrastructure.metal);
             div_information_energie = this.createOrUpdateElement("div", `div-${prefix}-energie-${infrastructure.id}`, "div-infrastructure-energie", "Energie: " + infrastructure.energie);
+
+
+            let div_upgrade = this.createOrUpdateElement("div", `div-${prefix}-upgrade-${infrastructure.id}`, "div-infrastructure-upgrade");
+            let button_upgrade = this.createOrUpdateElement(
+                "button",
+                `upgrade-button-${prefix}-${infrastructure.id}`,
+                "upgrade-button",
+                infrastructure.level === 0 ? "Construire <br>" + infrastructure.temps + "s" : "Améliorer <br> " + infrastructure.temps + "s"
+            );
+
+            button_upgrade.addEventListener("click", () =>
+            {
+                this.#controller.upgradeInfrastructure(infrastructure.id);
+            });
+
+            div_image.appendChild(img);
+            div_information.appendChild(div_information_type);
+            div_information.appendChild(div_information_level);
+            div_information.appendChild(div_information_metal);
+            div_information.appendChild(div_information_energie);
+            div_upgrade.appendChild(button_upgrade);
+
+            div.appendChild(div_image);
+            div.appendChild(div_information);
+            div.appendChild(div_upgrade);
+
+            document.getElementById(parentDivId).appendChild(div);
         }
         else if(prefix === "ressource") 
         {
+            let div_information_type = null;
+            let div_information_level = null;
+            let div_information_metal = null;
+            let div_information_energie = null;
+            let div_information_deuterium = null;
+
+            img.src = this.getImageSrcForType(infrastructure.type_ressource);
+
             div_information_type = this.createOrUpdateElement("div", `div-${prefix}-type-${infrastructure.id}`, "div-infrastructure-type", infrastructure.type_ressource);
             div_information_level = this.createOrUpdateElement("div", `div-${prefix}-level-${infrastructure.id}`, "div-infrastructure-level", "Niveau: " + infrastructure.level);
             div_information_metal = this.createOrUpdateElement("div", `div-${prefix}-metal-${infrastructure.id}`, "div-infrastructure-metal", "Métal: " + infrastructure.metal);
             div_information_energie = this.createOrUpdateElement("div", `div-${prefix}-energie-${infrastructure.id}`, "div-infrastructure-energie", "Energie: " + infrastructure.energie);
+
+
+            let div_upgrade = this.createOrUpdateElement("div", `div-${prefix}-upgrade-${infrastructure.id}`, "div-infrastructure-upgrade");
+            let button_upgrade = this.createOrUpdateElement(
+                "button",
+                `upgrade-button-${prefix}-${infrastructure.id}`,
+                "upgrade-button",
+                infrastructure.level === 0 ? "Construire <br>" + infrastructure.temps + "s" : "Améliorer <br> " + infrastructure.temps + "s"
+            );
+
+            button_upgrade.addEventListener("click", () =>
+            {
+                this.#controller.upgradeInfrastructure(infrastructure.id);
+            });
+
+            div_image.appendChild(img);
+            div_information.appendChild(div_information_type);
+            div_information.appendChild(div_information_level);
+            div_information.appendChild(div_information_metal);
+            div_information.appendChild(div_information_energie);
+            div_upgrade.appendChild(button_upgrade);
+
+            div.appendChild(div_image);
+            div.appendChild(div_information);
+            div.appendChild(div_upgrade);
+
+            document.getElementById(parentDivId).appendChild(div);
+        }
+        else if(prefix === "defense") 
+        {
+
+            let div_information_type = null;
+            let div_information_level = null;
+            let div_information_metal = null;
+            let div_information_energie = null;
+            let div_information_deuterium = null;
+            let div_point_attaque = null;
+            let div_point_defense = null;
+
+            img.src = this.getImageSrcForType(infrastructure.type_defense);
+
+            div_information_type = this.createOrUpdateElement("div", `div-${prefix}-type-${infrastructure.id}`, "div-infrastructure-type", infrastructure.type_defense);
+            div_information_level = this.createOrUpdateElement("div", `div-${prefix}-level-${infrastructure.id}`, "div-infrastructure-level", "Niveau: " + infrastructure.level);
+            div_information_metal = this.createOrUpdateElement("div", `div-${prefix}-metal-${infrastructure.id}`, "div-infrastructure-metal", "Métal: " + infrastructure.cout_metal);
+            div_information_energie = this.createOrUpdateElement("div", `div-${prefix}-energie-${infrastructure.id}`, "div-infrastructure-energie", "Energie: " + infrastructure.cout_energie);
+            div_information_deuterium = this.createOrUpdateElement("div", `div-${prefix}-deuterium-${infrastructure.id}`, "div-infrastructure-deuterium", "Deuterium: " + infrastructure.cout_deuterium);
+            div_point_attaque = this.createOrUpdateElement("div", `div-${prefix}-point-attaque-${infrastructure.id}`, "div-infrastructure-point-attaque", "Point d'attaque: " + infrastructure.point_attaque);
+            div_point_defense = this.createOrUpdateElement("div", `div-${prefix}-point-defense-${infrastructure.id}`, "div-infrastructure-point-defense", "Point de défense: " + infrastructure.point_defense);
+
+
+            let div_upgrade = this.createOrUpdateElement("div", `div-${prefix}-upgrade-${infrastructure.id}`, "div-infrastructure-upgrade");
+            let button_upgrade = this.createOrUpdateElement(
+                "button",
+                `upgrade-button-${prefix}-${infrastructure.id}`,
+                "upgrade-button",
+                infrastructure.level === 0 ? "Construire <br>" + infrastructure.temps_construction + "s" : "Améliorer <br> " + infrastructure.temps_construction + "s"
+            );
+
+            button_upgrade.addEventListener("click", () =>
+            {
+                this.#controller.upgradeInfrastructure(infrastructure.id);
+            });
+
+            div_image.appendChild(img);
+            div_information.appendChild(div_information_type);
+            div_information.appendChild(div_information_level);
+            div_information.appendChild(div_information_metal);
+            div_information.appendChild(div_information_energie);
+            div_information.appendChild(div_information_deuterium);
+            div_information.appendChild(div_point_attaque);
+            div_information.appendChild(div_point_defense);
+            div_upgrade.appendChild(button_upgrade);
+
+            div.appendChild(div_image);
+            div.appendChild(div_information);
+            div.appendChild(div_upgrade);
+
+            document.getElementById(parentDivId).appendChild(div);
         }
 
-        let div_upgrade = this.createOrUpdateElement("div", `div-${prefix}-upgrade-${infrastructure.id}`, "div-infrastructure-upgrade");
-        let button_upgrade = this.createOrUpdateElement(
-            "button",
-            `upgrade-button-${prefix}-${infrastructure.id}`,
-            "upgrade-button",
-            infrastructure.level === 0 ? "Construire <br>" + infrastructure.temps + "s" : "Améliorer <br> " + infrastructure.temps + "s"
-        );
+        
 
-        button_upgrade.addEventListener("click", () =>
-        {
-            this.#controller.upgradeInfrastructure(infrastructure.id);
-        });
-
-        div_image.appendChild(img);
-        div_information.appendChild(div_information_type);
-        div_information.appendChild(div_information_level);
-        div_information.appendChild(div_information_metal);
-        div_information.appendChild(div_information_energie);
-        div_upgrade.appendChild(button_upgrade);
-
-        div.appendChild(div_image);
-        div.appendChild(div_information);
-        div.appendChild(div_upgrade);
-
-        document.getElementById(parentDivId).appendChild(div);
+        
     }
 
     createOrUpdateElement(tagName, id, className, innerHTML = "") 
