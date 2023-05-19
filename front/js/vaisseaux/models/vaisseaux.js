7b91a2a873a86c7544bf831bb80640ee7612a39c
Romeo-mz <romeo_martinez@etu.u-bourgogne.fr>
2023-05-19 14:01:35 +0200

    add models

--- front/js/vaisseaux/models/vaisseaux.js
+++ front/js/vaisseaux/models/vaisseaux.js
@@ -1,4 +1,15 @@
 export class Vaisseau{
     #id;
+    #type;
     
+    constructor(id, type){
+        this.#id = id;
+        this.#type = type;
+    }
+
+    get id() { return this.#id; }
+    get type() { return this.#type; }
+
+    set id(id) { this.#id = id; }
+    set type(type) { this.#type = type; }
 }
