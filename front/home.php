f17610e0d8110a5b8a2456fd79d8d1ba781f5206
Goncalves Hugo <hugo.gclvs@icloud.com>
2023-05-27 18:19:41 +0200

    add home page

---
+++ front/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home.css">
+    <title>EsiremPire - Home Page</title>
+</head>
+<body>
+    
+</body>
+</html>
