b52d3e37d9bc27b1b9323fa9fb094784e19f6789
Goncalves Hugo <hugo.gclvs@icloud.com>
2023-05-29 12:49:56 +0200

    add session to attack and fleet pages

--- front/js/vaisseaux/application/application.js
+++ front/js/vaisseaux/application/application.js
@@ -1,23 +1,30 @@
 import { Controller } from "../controllers/controller.js";
 import { View } from "../views/view.js";
+import sessionDataService from '../../SessionDataService.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const myController = new Controller();
 
-    myController.loadVaisseaux()
-        .then(() => {
-            console.log("Success to load all vaisseaux")
-            const myView = new View(myController);
-            myController.loadFlotte()
-                .then(() => {
-                    console.log("Success to load flotte")
-                    myController.notify();
-                })
-                .catch(error => {
-                    alert("Error while loading vaisseaux - please refresh the page")
-                });
-        })
-        .catch(error => {
-            alert("Error while loading default vaisseaux - please refresh the page")
-        });
+    if (sessionDataService.getSessionData() !== null) {
+        const myController = new Controller();
+
+        myController.loadVaisseaux()
+            .then(() => {
+                console.log("Success to load vaisseaux")
+                const myView = new View(myController);
+                // myController.loadFlotte()
+                //     .then(() => {
+                //         console.log("Success to load flotte")
+                //         myController.notify();
+                //     })
+                //     .catch(error => {
+                //         alert("Error while loading flotte - please refresh the page")
+                //     });
+            })
+            .catch(error => {
+                alert("Error while loading vaisseaux - please refresh the page")
+            });
+    }
+    else
+        window.location.href = './login.php';
+        
 });
